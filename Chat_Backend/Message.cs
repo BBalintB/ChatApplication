1a4f700d453a86dde9e9d7e2a085f574a7a91d2f
hallgato <hallgato@LABOR215-17>
2022-03-22 16:58:59 +0100

    Add project files.

---
+++ Chat_Backend/Message.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat_Backend
+{
+    public class Message
+    {
+        private string content;
+
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+
+    }
+}
