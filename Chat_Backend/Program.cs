328ac66a7f191763ead6837b8e2be59bd12bfb26
bbalintb <bbalintb@gmail.com>
2022-03-22 18:32:42 +0100

    Multiple changed

--- Chat_Backend/Program.cs
+++ Chat_Backend/Program.cs
@@ -20,7 +20,8 @@ namespace Chat_Backend
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://*:22222");
+                    
                 });
     }
 }
