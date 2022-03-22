cd96aa9c26a4294c5d0384b7001809b056cb5d69
mark <erenyi.ate@gmail.com>
2022-03-22 17:42:57 +0100

    ok

---
+++ Chat_Client/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Chat_Backend;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Chat_Client
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        
+        public RestCollection<Message> Messages { get; set; }
+        
+        public ICommand Send { get; set; }
+
+        
+
+    }
+}
